50ae47b754be2bcd0add89a7344121b917c17a94
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-30 13:19:56 +0300

    feat: Fetch test for backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect } from 'react';
+
+
+
 import './App.css';
 import { Paths } from './Components/Routes';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+    .then(r => r.ok? r.json() : console.log(r))
+    .then(d => console.log(d))
+    .catch(e => alert(e))
+  },[])
+
   return (
-    <div 
-      className="App font-text text-2xl min-h-screen min-w-screen justify-center items-center
-      bg-gradient-to-bl from-slate-300 via-gray-400 to-gray-500 flex flex-col text-amber-800">
+    <div
+      className="App font-main text-2xl min-h-screen min-w-screen justify-center items-center
+      flex flex-col text-slate-300 bg-gradient-to-t from-slate-600 via-zinc-800 to-zinc-900">
       <Paths />
     </div>
   );
