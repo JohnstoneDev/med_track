914c30b91e53acc380d33ef5a848feb12421d965
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-07-03 23:30:52 +0300

    style: Spacing, background & text color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import { useEffect } from 'react';
 
-
-
-import './App.css';
 import { Paths } from './Components/Routes';
 
 function App() {
+
   useEffect(() => {
     fetch('http://localhost:3000/')
-    .then(r => r.ok? r.json() : console.log(r))
+    .then(r => r.ok? console.log("Server is Up!") : console.log(r))
     .then(d => console.log(d))
     .catch(e => alert(e))
   },[])
 
   return (
-    <div
-      className="App font-main text-2xl min-h-screen min-w-screen justify-center items-center
-      flex flex-col text-yellow-200 bg-teal-900">
-      <h1 className='font-headings text-5xl pb-5'>General Kenobi </h1>
-      <Paths />
-    </div>
+      <div
+        className="font-main text-2xl min-h-screen min-w-screen items-center space-y-4 bg-neutral-900 text-white overflow-hidden">
+        <Paths />
+      </div>
   );
 }
 
