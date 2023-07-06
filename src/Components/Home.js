450f09f6d777f4b33ae93af6e1e7780f0407afdd
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-07-06 23:27:36 +0300

    feat: Dummy content, create a logout method that dispatches an action to the reducer from the ApplicationContext

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,63 @@
+import {
+	useContext,
+	useEffect,
+	useState
+ } from "react"
 
+import { 
+	Link,
+	useNavigate
+ } from "react-router-dom";
+
+import { ApplicationContext } from "../App"
 
 export const Home = () => {
+	const { user, loggedIn } = useContext(ApplicationContext).state;
+	const { dispatch } = useContext(ApplicationContext)
+	const [ loading, setLoading ] = useState(true)
+
+	const navigate = useNavigate()
+
+	const logOut = () => {
+		console.log("LOg Out Clicked!")
+		fetch('/logout',
+		{
+			method : 'DELETE'
+		})
+		.then(() => {
+			dispatch({
+				type :'LOGOUT'
+			})
+			navigate("/login")
+		})
+	}
+
+	console.log(user)
+
+	setTimeout(() => {
+		setLoading(false)
+	},2000);
+
 	return (
-		<>
-				This is the Home Page
-		</>
+		<div className="text-center space-y-4">
+
+			{
+				loading ?
+				<div> Loading ... </div> :
+				<div>
+					<button hidden={!loggedIn} onClick={() => logOut()}>
+						Logout
+					</button>
+					{
+						loggedIn ? 
+						<h1>Hello {user.username}</h1> : 
+						<h1>You need to 
+							<Link to="/login">log in </Link> or <Link to="/signup">Sign Up</Link>
+						</h1>
+	
+					}
+				</div>
+			}
+		</div>
 	)
 }
