9bd35a860ce03dc61b8728c655de201b8c56dc54
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-07-06 23:23:20 +0300

    feat: Create Application context & initial State. Use a reducer to edit state using actions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
-import { useEffect } from 'react';
+import {
+  useEffect,
+  useReducer,
+  createContext,
+ } from 'react';
+
+ import {
+  useNavigate
+ } from "react-router-dom"
 
 import { Paths } from './Components/Routes';
 
+export const ApplicationContext = createContext();
+
+const initialState = {
+  loggedIn : false,
+  user : null
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SIGNUP' :
+      return {
+        ...state,
+        loggedIn : true,
+        user : action.payload
+      }
+    case 'LOGIN' :
+      return {
+        ...state,
+        loggedIn : true,
+        user : action.payload
+      };
+    case 'LOGOUT' :
+      return {
+        ...state,
+        loggedIn : false,
+        user : null
+      };
+    default :
+      return state
+  }
+}
+
+
 function App() {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('http://localhost:3000/')
-    .then(r => r.ok? console.log("Server is Up!") : console.log(r))
-    .then(d => console.log(d))
-    .catch(e => alert(e))
-  },[])
+    fetch('/me').then(r => {
+      if (r.ok) {
+        r.json().then(user => {
+          dispatch({
+            type : 'LOGIN',
+            payload : user
+          })
+          navigate('/')
+        })
+      }
+    })
+  },[navigate])
 
   return (
       <div
         className="font-main text-2xl min-h-screen min-w-screen items-center space-y-4 bg-neutral-900 text-white overflow-hidden">
-        <Paths />
+          <ApplicationContext.Provider value={{ state, dispatch}}>
+             <Paths />
+          </ApplicationContext.Provider>
       </div>
   );
 }
