3ac0e285a1c743b2ddacf4861734673cb9365688
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-07-07 14:21:07 +0300

    feat: Account page that will serve as the main display of a logged in user

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,9 +1,36 @@
+import {
+	useContext
+} from 'react'
 
+import { ApplicationContext } from '../App'
 
-export const Account = () => {
+export const Account = ( { user, loggedIn }) => {
+	const { dispatch } = useContext(ApplicationContext)
+
+	const logOut = () => {
+		fetch('/logout',
+		{
+			method : 'DELETE'
+		})
+		.then(() => {
+			dispatch({
+				type :'LOGOUT'
+			})
+		})
+	}
+
 	return (
-		<>
-			This is the Accounts Page
-		</>
+		<div className='p-4'>
+			<div className='flex justify-evenly items-center font-headings'>
+				<h1 className='border-b-4 border-b-blue-500'>
+					Hello there, <span className='text-amber-500 font-extrabold font-text'>{user.username} </span>!</h1>
+				<button
+					hidden={!loggedIn}
+					className="form-input w-[100px] rounded-md p-2 bg-blue-500 font-semibold hover:bg-amber-500 hover:transition-all duration-700"
+					onClick={logOut}>
+							Log Out
+				</button>
+			</div>
+		</div>
 	)
 }
