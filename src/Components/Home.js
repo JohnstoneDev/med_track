43c1354bf7a86fbee1cec72b769519528ad199a5
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-07-07 14:23:32 +0300

    feat: Render page content conditionally, move relevant code to the Account page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,42 +1,19 @@
 import {
 	useContext,
-	useEffect,
 	useState
  } from "react"
 
-import { 
-	Link,
-	useNavigate
- } from "react-router-dom";
-
 import { ApplicationContext } from "../App"
+import { NoUser } from "./NoUserDisplay";
+import { Account } from "./Account";
 
 export const Home = () => {
 	const { user, loggedIn } = useContext(ApplicationContext).state;
-	const { dispatch } = useContext(ApplicationContext)
 	const [ loading, setLoading ] = useState(true)
-
-	const navigate = useNavigate()
-
-	const logOut = () => {
-		console.log("LOg Out Clicked!")
-		fetch('/logout',
-		{
-			method : 'DELETE'
-		})
-		.then(() => {
-			dispatch({
-				type :'LOGOUT'
-			})
-			navigate("/login")
-		})
-	}
-
-	console.log(user)
 
 	setTimeout(() => {
 		setLoading(false)
-	},2000);
+	},1000);
 
 	return (
 		<div className="text-center space-y-4">
@@ -45,16 +22,10 @@ export const Home = () => {
 				loading ?
 				<div> Loading ... </div> :
 				<div>
-					<button hidden={!loggedIn} onClick={() => logOut()}>
-						Logout
-					</button>
 					{
-						loggedIn ? 
-						<h1>Hello {user.username}</h1> : 
-						<h1>You need to 
-							<Link to="/login">log in </Link> or <Link to="/signup">Sign Up</Link>
-						</h1>
-	
+						loggedIn ?
+						<Account user={user} loggedIn={loggedIn}/>:
+						<NoUser />
 					}
 				</div>
 			}
